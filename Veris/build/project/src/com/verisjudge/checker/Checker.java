539a1447cec1a5179040a10933a02973bd7092f8
Timothy Buzzelli <tbuzzelli@melbourne.ptl.ucf.edu>
2018-04-06 13:02:16 -0400

    Removed build and bin

--- Veris/build/project/src/com/verisjudge/checker/Checker.java
+++
@@ -1,7 +0,0 @@
-package com.verisjudge.checker;
-import com.verisjudge.Verdict;
-import com.verisjudge.utils.FastScanner;
-
-public abstract class Checker {
-    public abstract Verdict check(FastScanner input, FastScanner pScanner, FastScanner ansScanner);
-}
