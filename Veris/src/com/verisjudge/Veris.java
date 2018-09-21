5a55c086ca7f117b33d9ccda59609da99e92ab57
tbuzzelli <tbuzzelli@tbuzzelli-Inspiron-7558>
2018-09-21 09:11:45 -0400

    Remove BoxWriter and old Verdict class variables.

--- Veris/src/com/verisjudge/Veris.java
+++ Veris/src/com/verisjudge/Veris.java
@@ -1,7 +1,5 @@
 package com.verisjudge;
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
