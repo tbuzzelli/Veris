b6d3e4acedcb485fec9193b2bf817cf98c7eb00d
tbuzzelli <tbuzzelli@tbuzzelli-Inspiron-7558>
2018-09-23 09:37:12 -0400

    Added input validation to language spec editor.

--- Veris/src/com/verisjudge/ui/LanguageSpecEditorController.java
+++ Veris/src/com/verisjudge/ui/LanguageSpecEditorController.java
@@ -1,13 +1,14 @@
 package com.verisjudge.ui;
 
 import java.util.Arrays;
-import java.util.List;
+import java.util.Collections;
 import java.util.stream.Collectors;
 
 import com.verisjudge.Config;
 import com.verisjudge.LanguageSpec;
 import com.verisjudge.Main;
 
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -20,17 +21,19 @@ import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class LanguageSpecEditorController {
-	@FXML private Button buttonSave;
 
 	@FXML private TextField textFieldLanguageName;
 	@FXML private TextField textFieldFileExtensions;
-	@FXML private TextField textFieldAutoDetectLanguagePriority;
+	@FXML private TextField textFieldDetectLanguagePriority;
 
 	@FXML private TextArea textAreaCompileCommand;
 	@FXML private TextArea textAreaExecutionCommand;
 	
 	@FXML private CheckBox checkBoxIsAllowed;
 	@FXML private CheckBox checkBoxNeedsCompile;
+	
+	@FXML private Button buttonCancel;
+	@FXML private Button buttonSave;
 
 	private Stage stage;
 	
@@ -66,8 +69,106 @@ public class LanguageSpecEditorController {
 	@FXML
     protected void initialize() {
 		loadFromLanguageSpec(Config.getConfig().getLanguageSpecs()[0]);
+		
+		setUpInputValidation();
+	}
+	
+	private void setUpInputValidation() {
+		textFieldLanguageName.textProperty().addListener((o, ov, nv) -> validateLanguageName());
+		textFieldFileExtensions.textProperty().addListener((o, ov, nv) -> validateFileExtensions());
+		textAreaCompileCommand.textProperty().addListener((o, ov, nv) -> validateCompileCommand());
+		textAreaExecutionCommand.textProperty().addListener((o, ov, nv) -> validateExecutionCommand());
+		checkBoxNeedsCompile.selectedProperty().addListener((o, ov, nv) -> validateCompileCommand());
+		textFieldDetectLanguagePriority.textProperty().addListener((o, ov, nv) -> validateDetectLanguagePriority());
+		validateLanguageName();
+		validateFileExtensions();
+		validateCompileCommand();
+		validateExecutionCommand();
+		validateDetectLanguagePriority();
+	}
+	
+	private void validateLanguageName() {
+		ObservableList<String> styleClass = textFieldLanguageName.getStyleClass();
+        if (!isLanguageNameValid()) {
+            if (!styleClass.contains("error")) {
+                styleClass.add("error");
+            }
+        } else {
+            // remove all occurrences:
+            styleClass.removeAll(Collections.singleton("error"));                    
+        }
+        updateSaveButton();
+	}
+	
+	private void validateFileExtensions() {
+		ObservableList<String> styleClass = textFieldFileExtensions.getStyleClass();
+		System.err.println(getFileExtensions().length + "  " + Arrays.toString(getFileExtensions()));
+        if (!isFileExtensionsValid()) {
+            if (!styleClass.contains("error")) {
+                styleClass.add("error");
+            }
+        } else {
+            // remove all occurrences:
+            styleClass.removeAll(Collections.singleton("error"));                    
+        }
+        updateSaveButton();
 	}
 
+	private void validateCompileCommand() {
+		ObservableList<String> styleClass = textAreaCompileCommand.getStyleClass();
+        if (!isCompileCommandValid()) {
+            if (!styleClass.contains("error")) {
+                styleClass.add("error");
+            }
+        } else {
+            // remove all occurrences:
+            styleClass.removeAll(Collections.singleton("error"));                    
+        }
+        updateSaveButton();
+	}
+	
+	private void validateExecutionCommand() {
+		ObservableList<String> styleClass = textAreaExecutionCommand.getStyleClass();
+        if (!isExecutionCommandValid()) {
+            if (!styleClass.contains("error")) {
+                styleClass.add("error");
+            }
+        } else {
+            // remove all occurrences:
+            styleClass.removeAll(Collections.singleton("error"));                    
+        }
+        updateSaveButton();
+	}
+
+	private void validateDetectLanguagePriority() {
+		ObservableList<String> styleClass = textFieldDetectLanguagePriority.getStyleClass();
+        if (!isDetectLanguagePriorityValid()) {
+            if (!styleClass.contains("error")) {
+                styleClass.add("error");
+            }
+        } else {
+            // remove all occurrences:
+            styleClass.removeAll(Collections.singleton("error"));                    
+        }
+        updateSaveButton();
+	}
+
+	private void updateSaveButton() {
+		if (isLanguageNameValid()
+				&& isFileExtensionsValid()
+				&& isCompileCommandValid()
+				&& isExecutionCommandValid()
+				&& isDetectLanguagePriorityValid()) {
+			buttonSave.setDisable(false);
+		} else {
+			buttonSave.setDisable(true);
+		}
+	}
+
+	@FXML protected void handleCancelButtonAction(ActionEvent event) {
+		event.consume();
+	}
+	
 	@FXML protected void handleSaveButtonAction(ActionEvent event) {
 		System.err.println(createLanguageSpec());
 		
@@ -81,37 +182,75 @@ public class LanguageSpecEditorController {
 				.setCompileArgs(getCompileArgs())
 				.setExecutionArgs(getExecutionArgs())
 				.setDetectLanguagePriority(getDetectLanguagePriority())
-				.setIsAllowed(checkBoxIsAllowed.isSelected())
-				.setNeedsCompile(checkBoxNeedsCompile.isSelected())
+				.setIsAllowed(getIsAllowed())
+				.setNeedsCompile(getNeedsCompile())
 				.build();
+	}
+	
+	private boolean isLanguageNameValid() {
+		return !getLanguageName().isEmpty();
+	}
+
+	private String getLanguageName() {
+		return textFieldLanguageName.getText();
+	}
+	
+	private boolean getIsAllowed() {
+		return checkBoxIsAllowed.isSelected();
+	}
+	
+	private boolean getNeedsCompile() {
+		return checkBoxNeedsCompile.isSelected();
+	}
+
+	private boolean isCompileCommandValid() {
+		return !getNeedsCompile() || getCompileArgs().length > 0;
 	}
 	
 	private String[] getCompileArgs() {
 		return LanguageSpec.convertStringToArgsList(textAreaCompileCommand.getText()).toArray(new String[0]);
 	}
 
+	private boolean isExecutionCommandValid() {
+		return getExecutionArgs().length > 0;
+	}
+
 	private String[] getExecutionArgs() {
 		return LanguageSpec.convertStringToArgsList(textAreaExecutionCommand.getText()).toArray(new String[0]);
 	}
+	
+	private boolean isFileExtensionsValid() {
+		return getFileExtensions().length > 0;
+	}
 
 	private String[] getFileExtensions() {
-		return textFieldFileExtensions.getText().split("(\\s|,)+");
+		return Arrays.stream(textFieldFileExtensions.getText().split("(\\s|,)+"))
+				.map(a -> a.replace(",", "").replace(" ", ""))
+				.filter(a -> !a.isEmpty())
+				.toArray(String[]::new);
 	}
 	
+	private boolean isDetectLanguagePriorityValid() {
+		try {
+			Long.parseLong(textFieldDetectLanguagePriority.getText());
+			return true;
+		} catch (NumberFormatException e) {
+			return false;
+		}
+	}
+
 	private long getDetectLanguagePriority() {
 		try {
-			return Long.parseLong(textFieldAutoDetectLanguagePriority.getText());
+			return Long.parseLong(textFieldDetectLanguagePriority.getText());
 		} catch (NumberFormatException e) {
-			// TODO: Somehow use this error or don't use the result when we would get an error.
-			e.printStackTrace();
-			return 0;
+			return 1L;
 		}
 	}
 	
 	private void loadFromLanguageSpec(LanguageSpec languageSpec) {
 		textFieldLanguageName.setText(languageSpec.getLanguageName());
 		textFieldFileExtensions.setText(Arrays.stream(languageSpec.getFileExtensions()).collect(Collectors.joining(", ")));
-		textFieldAutoDetectLanguagePriority.setText("" + languageSpec.getDetectLanguagePriority());
+		textFieldDetectLanguagePriority.setText("" + languageSpec.getDetectLanguagePriority());
 
 		textAreaCompileCommand.setText(Arrays.stream(languageSpec.getCompileArgs()).collect(Collectors.joining(" ")));
 		textAreaExecutionCommand.setText(Arrays.stream(languageSpec.getExecutionArgs()).collect(Collectors.joining(" ")));
